f583594cc6e77739ebd3f96daa65ffc2a71b1887
ramzimh <ramzimahnoune@gmail.com>
2020-04-04 04:18:13 +0100

    adding new items to the toDo list

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 
 var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+var todoItems = ["hi", "cv"];
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", function (req, res) {
     var date = new Date();
-  
- var options={
-weekday:"long",
-day :"numeric",
-month:"long"
- }
- var day=date.toLocaleDateString("en-US",options);
- 
-    res.render("list", { kindOfDay: day });
-})
 
-app.post("/",function(req,res){
-    todoItem=req.body.todo;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = date.toLocaleDateString("en-US", options);
+
+    res.render("list", { today: day, newItem: todoItems });
+});
+
+app.post("/", function (req, res) {
+    var todoItem = req.body.todo;
+    todoItems.push(todoItem);
     console.log(todoItem);
-})
+    res.redirect("/");
+});
 
 app.listen(3000, function () {
     console.log("app started");
