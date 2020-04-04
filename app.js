0c62e40e236f601b0b835c4670cf3565d0cdd4fb
ramzimh <ramzimahnoune@gmail.com>
2020-04-04 03:00:43 +0100

    retrieve entered items

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var request = require("request");
+
 
 var app = express();
-app.set("viw engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    
+    var date = new Date();
+  
+ var options={
+weekday:"long",
+day :"numeric",
+month:"long"
+ }
+ var day=date.toLocaleDateString("en-US",options);
+ 
+    res.render("list", { kindOfDay: day });
 })
 
-
+app.post("/",function(req,res){
+    todoItem=req.body.todo;
+    console.log(todoItem);
+})
 
 app.listen(3000, function () {
     console.log("app started");
